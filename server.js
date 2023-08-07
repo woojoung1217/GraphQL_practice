37174f603a77491312234e8468c5edb33c74844b
woojoung <ywj981217@naver.com>
2023-08-07 18:18:18 +0900

    mutation with server

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  {
+    id: "1",
+    text: "first one",
+  },
+  {
+    id: "2",
+    text: "second one",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID
@@ -10,7 +21,7 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
@@ -22,7 +33,36 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: String(tweets.length + 1), // ID는 String 또는 ID 타입으로 변환해야 합니다.
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweetIndex = tweets.findIndex((tweet) => tweet.id === id);
+      if (tweetIndex !== -1) {
+        tweets.splice(tweetIndex, 1);
+        return true;
+      }
+      return false;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`runing on ${url}`);
