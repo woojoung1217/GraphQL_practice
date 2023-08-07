10843b6e88923a27292051817f32450301913b8f
woojoung <ywj981217@naver.com>
2023-08-07 17:30:26 +0900

    "mutaiton"

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ import { ApolloServer, gql } from "apollo-server";
 const typeDefs = gql`
   type User {
     id: ID
-    username: String
+    username: String!
+    firstName: String!
+    lastName: String!
   }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
